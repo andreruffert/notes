e35234cc8cd9d96718b5a72344c5759b7e121f64
André Ruffert <andre@andreruffert.com>
2015-03-05 00:09:15 +0100

    feature: add autofocus, settings, github link, refactor
    * Autofocus onload
    * Has now a github link to the repo
    * Possibility for settings for now invert colors only to switch to a
    dark mode

--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
 (function() {
 
-  if (!localStorage.getItem('notes')) {
-    localStorage.setItem('notes', JSON.stringify([]));
-  }
+  var defaults = {
+    settings: {
+      invertColors: false
+    }
+  };
+
+  populateStorage('settings', defaults.settings);
+  populateStorage('notes', []);
+
 
   var editor = document.getElementById('js-editor');
+  var settings = JSON.parse(localStorage.getItem('settings'));
   var notes = JSON.parse(localStorage.getItem('notes'));
 
+  // initialize settings
+  setSettings(settings);
+
+  // load notes into editor
   editor.value = notes.join('\n');
 
   editor.addEventListener('input', function(e) {
-    var lines = this.value.split('\n');
-    var lineNumber = this.value.substr(0, this.selectionStart).split('\n').length;
+    updateNotes(this, notes);
+  }, false);
+
+
+  /**
+   * updateNotes
+   * @param  {Element} editor
+   * @param  {Array}   notes
+   */
+  function updateNotes(editor, notes) {
+    var lines = editor.value.split('\n');
+    var lineNumber = editor.value.substr(0, editor.selectionStart).split('\n').length;
     var lineContent = lines[lineNumber - 1];
 
-    // update notes
     if (lineContent === '' && lineNumber === lines.length) {
       notes.splice(lineNumber - 1, 1);
     }
@@ -24,6 +44,32 @@
 
     // update localStorage
     localStorage.setItem('notes', JSON.stringify(notes));
-  }, false);
+  }
+
+  /**
+   * setSettings
+   * @param  {Object} settings
+   */
+  function setSettings(settings) {
+    var name, value;
+    for (var key in settings) {
+      if (settings.hasOwnProperty(key)) {
+        name = 'data-' + key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+        value = settings[key];
+        document.documentElement.setAttribute(name, value);
+      }
+    }
+  }
+
+  /**
+   * populateStorage
+   * @param  {String} key
+   * @param  {Object} props
+   */
+  function populateStorage(key, props) {
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, JSON.stringify(props));
+    }
+  }
 
 })();
